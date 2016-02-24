6bd6b8c6dbb4dc32f79fea4e37bff2abdfa9d1db
Mats Henricson <mts.henricson@magine.com>
2016-02-24 16:16:56 +0100

    Just some changes

--- java/tdd-training/src/main/java/Primes.java
+++ java/tdd-training/src/main/java/Primes.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class Primes {
 
 
-    public static int[] of(int i) {
-        return new int[]{1};
+    public static List<Integer> of(int i) {
+        if (i == 1) return Arrays.asList(1);
+        if (i == 2) return Arrays.asList(2);
+        if (i == 4) return Arrays.asList(2, 2);
+        else return Arrays.asList(3);
     }
 }
--- java/tdd-training/src/test/java/PrimesTest.java
+++ java/tdd-training/src/test/java/PrimesTest.java
@@ -1,11 +1,15 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class PrimesTest {
 
     @Test
     public void prime_of_1() throws Exception {
-        int[] expected = new int[]{1};
-        Assert.assertArrayEquals(expected, Primes.of(1));
+        Assert.assertEquals(Arrays.asList(1), Primes.of(1));
+        Assert.assertEquals(Arrays.asList(2), Primes.of(2));
+        Assert.assertEquals(Arrays.asList(3), Primes.of(3));
+        Assert.assertEquals(Arrays.asList(2, 2), Primes.of(4));
     }
 }
